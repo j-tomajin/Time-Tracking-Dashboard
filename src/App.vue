374b1fd0f0872e67d876aea9053941d167f4d5c3
Jerickson Tomajin <tomajinj@gmail.com>
2023-07-27 21:04:00 +0800

    app main

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<script>
+  import Profile from './components/Profile.vue';
+  import Events from './components/Events.vue';
+  export default {
+    name: 'App',
+    components: {
+      Events,
+      Profile
+    }
+  }
+</script>
+
+<template>
+  <main class="main">
+    <Profile></Profile>
+    <Events></Events>
+  </main>
+</template>
+
+<style lang="scss" scoped>
+  @use './assets/scss/utilities/' as *;
+
+  .main {
+    width: min(calc(100% - 32px), 1200px);
+    margin-inline: auto;
+    gap: rem(32);
+
+    display: grid;
+    grid-template-columns: 1fr;
+
+    @include breakpointMin(large-screen) {
+      grid-template-columns: .5fr 2fr;
+      place-content: center;
+    }
+  }
+</style>
